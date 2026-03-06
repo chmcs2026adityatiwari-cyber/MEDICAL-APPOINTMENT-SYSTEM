614811c1e0d0344cafd899dabbe13478d4c660de
Adiii <chmcs2026.aditya.tiwari@gmail.com>
2026-03-07 03:01:21 +0530

    fixed axios api config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const api = axios.get({
-  //baseURL: "http://localhost:3001/api"
-  baseURL: "https://medical-appointment-system-xwh9.onrender.com/api/appointments"
+const api = axios.create({
+  baseURL: "https://medical-appointment-system-xwh9.onrender.com/api"
 });
-
 
 export default api;
