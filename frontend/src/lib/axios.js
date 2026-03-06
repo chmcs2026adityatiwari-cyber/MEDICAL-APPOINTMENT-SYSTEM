9189b98466faa57f7a37e7d6332aadae3df7499c
Adiii <chmcs2026.aditya.tiwari@gmail.com>
2026-03-07 02:35:22 +0530

    fixed api url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "http://localhost:3001/api"
-  baseURL: "https://medical-appointment-system-1-zid3.onrender.com"
+  baseURL: "https://your-backend-name.onrender.com/api/appointments"
 });
 
 
